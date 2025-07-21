60e69a03d9c39f06fa6891910529e5b60fce0679
lx2733@outlook.com <lx2733@outlook.com>
2025-07-21 17:28:28 +0800

    Refactor GameManager class to encapsulate game state and audio handling

--- PitchBalloon/sketch.js
+++ PitchBalloon/sketch.js
@@ -1,17 +1,22 @@
-let audioContext;
-let mic;
-let pitch;
-let balloon;
-let explode = false;
 const canvasWidth = 400;
 const canvasHeight = 400;
 const minBalloonWidth = 200;
 const maxBalloonHeight = 400;
-
 const minFrequency = 100;
 const maxFrequency = 2000;
 
-let particles = [];
+class GameManager {
+  constructor() {
+    this.audioContext = null;
+    this.mic = null;
+    this.pitch = null;
+    this.balloon = null;
+    this.explode = false;
+    this.particles = [];
+  }
+}
+
+let game = new GameManager();
 class Particle {
   constructor(x, y) {
     this.x = x;
@@ -20,13 +25,11 @@ class Particle {
     this.vy = random(-10, 10);
     this.life = 150;
   }
-  
   update() {
     this.x += this.vx;
     this.y += this.vy;
     this.life -= 5;
   }
-  
   show() {
     noStroke();
     ellipse(this.x, this.y, 16);
@@ -40,32 +43,27 @@ class Balloon {
     this.size = size;
     this.lightness = 90;
   }
-  
   display() {
     noStroke();
-     // 根据lightness值设置颜色
     colorMode(HSL);
-     // 添加渐变
-     let gradient = drawingContext.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.size / 2);
-     gradient.addColorStop(0, `hsl(0, 100%, ${this.lightness + 10}%)`);
-     gradient.addColorStop(1, `hsl(0, 100%, ${this.lightness}%)`);
-     drawingContext.fillStyle = gradient;
-     // 绘制气球
-     ellipse(this.x, this.y, this.size);
+    let gradient = drawingContext.createRadialGradient(this.x, this.y, 0, this.x, this.y, this.size / 2);
+    gradient.addColorStop(0, `hsl(0, 100%, ${this.lightness + 10}%)`);
+    gradient.addColorStop(1, `hsl(0, 100%, ${this.lightness}%)`);
+    drawingContext.fillStyle = gradient;
+    ellipse(this.x, this.y, this.size);
   }
 }
 
 function setup() {
-  balloon = new Balloon(canvasWidth/2, canvasWidth/2, minBalloonWidth);
-  // noCanvas();
+  game.balloon = new Balloon(canvasWidth/2, canvasWidth/2, minBalloonWidth);
   let canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.parent('canvas-container');
   select('#enable').mousePressed(function () {
     try {
-      audioContext = getAudioContext();
-      audioContext.resume()
-      mic = new p5.AudioIn();
-      mic.start(startPitch);
+      game.audioContext = getAudioContext();
+      game.audioContext.resume();
+      game.mic = new p5.AudioIn();
+      game.mic.start(startPitch);
       select('#enable').remove();
     } catch (err) {
       console.error(err);
@@ -76,29 +74,26 @@ function setup() {
 
 function draw() {
   background(255);
-
-  // 绘制气球
-  if (explode===true) {
-    // 气球爆炸动画
-    for (let i = particles.length - 1; i >= 0; i--) {
-      particles[i].update();
-      particles[i].show();
-      if (particles[i].life <= 0) {
-        particles.splice(i, 1);
+  if (game.explode === true) {
+    for (let i = game.particles.length - 1; i >= 0; i--) {
+      game.particles[i].update();
+      game.particles[i].show();
+      if (game.particles[i].life <= 0) {
+        game.particles.splice(i, 1);
       }
     }
-    if (particles.length === 0) {
-      explode = false;
-      balloon.size = minBalloonWidth;
-      balloon.lightness = 90;
+    if (game.particles.length === 0) {
+      game.explode = false;
+      game.balloon.size = minBalloonWidth;
+      game.balloon.lightness = 90;
     }
   } else {
-    balloon.display();
+    game.balloon.display();
   }
 }
 function startPitch() {
   select('#status').html('Loading Model');
-  pitch = ml5.pitchDetection('./model/', audioContext, mic.stream, modelLoaded);
+  game.pitch = ml5.pitchDetection('./model/', game.audioContext, game.mic.stream, modelLoaded);
 }
 
 function modelLoaded() {
@@ -107,18 +102,18 @@ function modelLoaded() {
 }
 
 function getPitch() {
-  pitch.getPitch(function (err, frequency) {
+  game.pitch.getPitch(function (err, frequency) {
     if (frequency) {
       select('#result').html(`频率为：${frequency}Hz`);
-      if(frequency>350){
-        explode = true;
+      if(frequency > 350){
+        game.explode = true;
         for (let i = 0; i < 100; i++) {
-          particles.push(new Particle(balloon.x, balloon.y));
+          game.particles.push(new Particle(game.balloon.x, game.balloon.y));
         }
       }
       let size = map(frequency, minFrequency, maxFrequency, minBalloonWidth, maxBalloonHeight);
-      balloon.lightness = map(frequency, minFrequency, maxFrequency, 90, 35);
-      balloon.size = size;
+      game.balloon.lightness = map(frequency, minFrequency, maxFrequency, 90, 35);
+      game.balloon.size = size;
     } else {
       select('#result').html("未检测到声音");
     }
